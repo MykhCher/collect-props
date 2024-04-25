c94a2c36118d008c5df1b25213e184c5dff21b9e
MykhCher <micher1240@gmail.com>
2024-04-25 04:17:17 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 'use strict';
 
-const email_input = document.querySelector('#email');
+function passwordValidation(e) {
+    // Get password inputs, initialize password verification pattern
+    const passPattern = /^(?=[\w@#$^&*+=]{8,20}$)\w+[@#$^&*+=]*/;
+    const passInput = document.querySelector('#password1');
+    const passConfirmInput = document.querySelector('#password2');
 
-// Add div with error message to a page 
-const error_msg = document.createElement('div');
-
-error_msg.setAttribute('class', 'error-msg');
-error_msg.innerHTML = '(!) You should enter a proper email into this field!';
-error_msg.style.display = 'none';
-
-document.getElementById('email-input-container').append(error_msg);
-
-// Add event listener
-email_input.addEventListener('change', (e) => {
-    const emailRegExp = /^\w+\.?\w+@[a-z]{3,8}\.[a-z]{2,5}$/;
-    if (emailRegExp.test(e.target.value)) {
-        error_msg.style.display = 'none';
-    } else {
-        error_msg.style.display = 'block';
-    }
-})
+    // Create div with error msg
+    const errorMsg = document.createElement('div');
+}

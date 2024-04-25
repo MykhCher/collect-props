60702145342f8e4e0e0ee3d65f09a11f4799ccf4
MykhCher <micher1240@gmail.com>
2024-04-25 03:31:43 +0300

    remove user creation code

--- src/index.js
+++ src/index.js
@@ -1,32 +1,6 @@
 'use strict';
 
-// Initialise class Person
-class Person {
-    constructor(fname, lname, nickname, email) {
-        this.fname = fname;
-        this.lname = lname;
-        this.nickname = nickname;
-        this.email = email;
-    }
-}
-
 const email_input = document.querySelector('#email');
-
-// Function to be an event listener.
-const createUser = (e) => {
-    e.preventDefault();
-    const fname = document.querySelector('#fname').value;
-    const lname = document.querySelector('#lname').value;
-    const nickname = document.querySelector('#nickname').value;
-    const email = email_input.value;
-    
-    if (fname && lname && nickname && email) {
-        const newUser = new Person(fname, lname, nickname, email);
-        localStorage.setItem(lname, JSON.stringify(newUser))
-    } else {
-        console.log(`Cannot create user without 1 of props`);
-    }
-} 
 
 // Add div with error message to a page 
 const error_msg = document.createElement('div');
@@ -37,9 +11,7 @@ error_msg.style.display = 'none';
 
 document.getElementById('email-input-container').append(error_msg);
 
-// Add event listener to a button.
-const submit_btn = document.querySelector('input[type="submit"]');
-
+// Add event listener
 email_input.addEventListener('change', (e) => {
     const emailRegExp = /^\w+\.?\w+@[a-z]{3,8}\.[a-z]{2,5}$/;
     if (emailRegExp.test(e.target.value)) {
@@ -48,5 +20,3 @@ email_input.addEventListener('change', (e) => {
         error_msg.style.display = 'block';
     }
 })
-
-const emailRegExp = new RegExp('^\w+\.?\w+@[a-z]{3,8}\.[a-z]{2,5}$', 'gi')

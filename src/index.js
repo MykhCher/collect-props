1d1d84be721cc884165fe2ea1530a3e5268756a9
MykhCher <micher1240@gmail.com>
2024-04-25 04:18:12 +0300

    renamed variables to camel case

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 'use strict';
 
-const email_input = document.querySelector('#email');
+const emailInput = document.querySelector('#email');
 
 // Add div with error message to a page 
-const error_msg = document.createElement('div');
+const errorMsg = document.createElement('div');
 
-error_msg.setAttribute('class', 'error-msg');
-error_msg.innerHTML = '(!) You should enter a proper email into this field!';
-error_msg.style.display = 'none';
+errorMsg.setAttribute('class', 'error-msg');
+errorMsg.innerHTML = '(!) You should enter a proper email into this field!';
+errorMsg.style.display = 'none';
 
-document.getElementById('email-input-container').append(error_msg);
+document.getElementById('email-input-container').append(errorMsg);
 
 // Add event listener
-email_input.addEventListener('change', (e) => {
+emailInput.addEventListener('change', (e) => {
     const emailRegExp = /^\w+\.?\w+@[a-z]{3,8}\.[a-z]{2,5}$/;
     if (emailRegExp.test(e.target.value)) {
-        error_msg.style.display = 'none';
+        errorMsg.style.display = 'none';
     } else {
-        error_msg.style.display = 'block';
+        errorMsg.style.display = 'block';
     }
 })
